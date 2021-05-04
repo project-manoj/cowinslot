86cc000b51f4f9743c20fed43b2ce7cf7d4ddb43
project-manoj <64952143+project-manoj@users.noreply.github.com>
2021-05-04 16:39:38 +0530

    Add files via upload

---
+++ Cowinslots.py
@@ -0,0 +1,121 @@
+# MIT License
+#
+# Copyright (c) [2021] [Manoj Kumar]
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import requests
+import json
+import datetime
+from pathlib import Path
+from bs4 import BeautifulSoup
+import smtplib
+import time
+import sys
+import os
+
+# Global file
+filename = "/tmp/slot18"
+
+# Send an email
+def send_email(msg_body):
+
+    user_email = "user1@gmail.com"
+    user_pass = "PASSWORD"
+    # Uncomment for using it on Ubuntu
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    # UnComment next two lines to use on CentOS
+    # server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    # server.ehlo()
+
+    server.login(user_email, user_pass)
+
+    subject = "Covid Slot Check"
+    msg = f"Subject: {subject}\n\n{msg_body}"
+    recipients = ["user2@gmail.com"]
+    # , ""]
+    server.sendmail(user_email, recipients, msg)
+
+    server.quit()
+
+
+# Get District wise slot availablity
+def getSlotsDistrict(district, date_now):
+
+    url_delhi = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={district}&date={date_now}"
+    response = requests.get(url_delhi).text
+
+    sessions = json.loads(response)["sessions"]
+    for session in sessions:
+        if (
+            float(session["min_age_limit"] < 45)
+            and float(session["available_capacity"]) > 1
+        ):
+            print("\n-------------------------------------------------------------")
+            print(
+                session["center_id"],
+                session["name"],
+                session["district_name"],
+                session["pincode"],
+                "\t",
+                session["date"],
+                " AGE LIMIT:",
+                session["min_age_limit"],
+                "Availability:",
+                session["available_capacity"],
+                session["vaccine"],
+            )
+
+
+# Main Program Starts from here ----->
+slotFile = open(filename, "w")
+original = sys.stdout
+sys.stdout = slotFile
+# check status of 6 days
+duration = 6
+date = datetime.datetime.now()
+email_body = []
+
+# district code for delhi is 9
+# check for your district code
+url_delhi = "https://cdn-api.co-vin.in/api/v2/admin/location/districts/9"
+response = requests.get(url_delhi).text
+
+
+for i in range(duration):
+    today = f"{date.day + i}-{date.month}-{date.year}"
+
+    districts = json.loads(response)["districts"]
+    for district in districts:
+        getSlotsDistrict(district["district_id"], today)
+
+sys.stdout = original
+slotFile.close()
+
+email_body = Path(filename).read_text()
+print(email_body)
+
+# Uncomment to send email to notify
+# if len(email_body) > 0:
+#  send_email(email_body)
+#
